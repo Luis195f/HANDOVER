57a2bc59e10ced842d71b09c2f2fc9e31a2602cf
Luis195f <84361808+Luis195f@users.noreply.github.com>
2025-11-04 08:11:33 +0100

    fix(babel): align expo nativewind v4 setup

--- babel.config.js
+++ babel.config.js
@@ -2,16 +2,17 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel',
+    ],
     plugins: [
       ['module-resolver', {
         root: ['./'],
         alias: { '@': './src', '@src': './src' },
         extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
       }],
-      // Usamos css-interop (NativeWind v4) y dejamos Reanimated al FINAL
-      'react-native-css-interop/babel',
-      'react-native-reanimated/plugin',
+      'react-native-reanimated/plugin', // siempre al final
     ],
   };
 };
