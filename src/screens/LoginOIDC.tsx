94793cffb25fa46de9a72777af377bdcb7f620a7
Luis195f <84361808+Luis195f@users.noreply.github.com>
2025-11-11 14:27:24 +0100

    chore(security/e2e/ci): rbac/login stub + maestro flow + ci artifacts

---
+++ src/screens/LoginOIDC.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function LoginOIDC(): JSX.Element {
+  return (
+    <View style={{ padding: 16 }}>
+      <Text>Login OIDC (stub)</Text>
+    </View>
+  );
+}
