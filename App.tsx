461c4bd8a25f51d4064869fa388848618911b277
Luis195f <84361808+Luis195f@users.noreply.github.com>
2025-11-16 23:26:33 +0100

    Wrap AppThemeProvider in AuthProvider

--- App.tsx
+++ App.tsx
@@ -5,15 +5,19 @@ import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "@/src/navigation/RootNavigator";
 import { AppThemeProvider } from "@/src/theme";
 import { navigationRef } from "@/src/navigation/navigation";
+import { AuthProvider } from "@/src/lib/auth/AuthContext";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <AppThemeProvider>
-        <NavigationContainer ref={navigationRef}>
-          <RootNavigator />
-        </NavigationContainer>
-      </AppThemeProvider>
+      <AuthProvider>
+        <AppThemeProvider>
+          <NavigationContainer ref={navigationRef}>
+            <RootNavigator />
+          </NavigationContainer>
+        </AppThemeProvider>
+      </AuthProvider>
     </SafeAreaProvider>
   );
 }
+
