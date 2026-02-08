ef4d30424035f634f6364205875f299dc5d6389d
Luis195f <84361808+Luis195f@users.noreply.github.com>
2026-02-08 09:22:29 +0100

    test(jest): mock expo crypto/filesystem and fix module mapping (#524)

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,10 +5,14 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest-tests/setup.ts'],
   roots: ['<rootDir>/jest-tests'],
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
-  moduleNameMapper: {
-    '^@/src/(.*)$': '<rootDir>/src/$1',
-  },
+
   transform: {
     '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+
+  moduleNameMapper: {
+    '^@/src/(.*)$': '<rootDir>/src/$1',
+    '^expo-file-system$': '<rootDir>/jest-tests/__mocks__/expo-file-system.js',
+    '^expo-crypto$': '<rootDir>/jest-tests/__mocks__/expo-crypto.js',
   },
 };
