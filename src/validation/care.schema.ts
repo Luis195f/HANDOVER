5e8848676e3d944bcaf5a453a9a1429297c75f72
Luis195f <84361808+Luis195f@users.noreply.github.com>
2026-03-04 13:24:01 +0100

    Update care.schema.ts

--- src/validation/care.schema.ts
+++ src/validation/care.schema.ts
@@ -1,9 +1,6 @@
-import { z } from 'zod';
-import { zHandoverObject } from './schemas';
+import { zHandoverObject } from "./schemas";
 
-const baseSchema = zHandover instanceof z.ZodEffects ? zHandover.innerType() : zHandover;
-
-export const careSchema = baseSchema.pick({
+export const careSchema = zHandoverObject.pick({
   meds: true,
   medications: true,
   treatments: true,
